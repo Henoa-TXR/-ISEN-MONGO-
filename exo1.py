ac019e1f311eaad0ddadd1c904c5c287f3e0d01b
Charles Fenart <charlesfenart@orange.fr>
2021-09-21 16:28:55 +0200

    Paris update

--- exo1.py
+++ exo1.py
@@ -13,9 +13,17 @@ myDB=myClient['Station']
 exo1_collection=myDB['Exo1']
 test_col=myDB['nom']
 
+#Suppression des données précédentes
+exo1_collection.drop()
+
 #Récupération des données des stations par ville
 
-#LILLE
+
+
+
+#               LILLE
+
+
 
 #Récupération de l'API
 url="https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
@@ -23,9 +31,6 @@ url="https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-r
 reponse = requests.get(url)
 contenu=reponse.json()
 infos_stations=contenu['records']
-
-#Suppression des données précédentes
-exo1_collection.drop()
 
 #Récupération des données de l'api
 tab=[]
@@ -36,3 +41,51 @@ for input_station in infos_stations:
 #Ajout des données dans la BDD
 exo1_collection.insert_many(tab)
 
+
+
+#                       PARIS
+
+
+
+#Pour les vélos parisiens, nous avons besoin de 2 API différentes
+#La récupération des données se fera donc en 2 étapes
+
+#Récupération de la première API (nom des stations, coordonnées)
+url="https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_information.json"
+
+reponse = requests.get(url)
+contenu=reponse.json()
+infos_stations1=contenu['data']['stations']
+
+#Récupération de la deuxième API (nombre de vélos et de places disponibles)
+url="https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_status.json"
+
+reponse = requests.get(url)
+contenu=reponse.json()
+infos_stations2=contenu['data']['stations']
+
+#Récupération des données spécifiques de la première api
+tab1=[]
+for input_station in infos_stations1:
+    input_tab=[input_station["name"],input_station["lat"],input_station["lon"]]
+    tab1.append(input_tab)
+
+#Récupération des données spécifiques de la deuxième api
+tab2=[]
+for input_station in infos_stations2:
+    if(input_station["is_renting"]==1):
+        etat="EN SERVICE"
+    else:
+        etat="HORS SERVICE"
+    input_tab=[input_station["num_bikes_available"],input_station["num_docks_available"],etat]
+    tab2.append(input_tab)
+
+#Concaténation des données provenant des 2 APIs
+if(len(tab1)==len(tab2)):
+    tab=[]
+    for i in range (0,len(tab1)):
+        input_json={"ville":"Paris","nomstation":tab1[i][0],"nombrevelo":tab2[i][0],"nombreplaces":tab2[i][1],"etat":tab2[i][2],"latitude":tab1[i][1],"longitude":tab1[i][2]}
+        tab.append(input_json)
+
+#Ajout des données dans la BDD
+exo1_collection.insert_many(tab)
