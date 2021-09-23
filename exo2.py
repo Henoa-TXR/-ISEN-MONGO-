e466017d756980aca8beb49d93130398db6cf8b0
Henoa <texiereloi@gmail.com>
2021-09-23 16:15:27 +0200

    Fin exo2 et exo3

--- exo2.py
+++ exo2.py
@@ -1,55 +1,37 @@
 # Import nécessaire
-from pymongo import MongoClient
+from pymongo import MongoClient, GEOSPHERE
 import pymongo
 import requests
 import time 
 
 def lille():
-        #Récupération de l'API
+    #Récupération de l'API
     url="https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=-1&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion"
 
     reponse = requests.get(url)
     contenu=reponse.json()
     infos_stations=contenu['records']
 
-    #Récupération des données de l'api
-    tab=[]
+    lilleTab1=[]
     for input_station in infos_stations:
-        input_json={"ville":"Lille","nomstation":input_station["fields"]["nom"],"nombrevelo":input_station["fields"]["nbvelosdispo"],"nombreplaces":input_station["fields"]["nbplacesdispo"],"etat":input_station["fields"]["etat"],"latitude":input_station["fields"]["localisation"][0],"longitude":input_station["fields"]["localisation"][1]}
-        tab.append(input_json)
-
-    #Ajout des données dans la BDD
-    exo2_collection.insert_many(tab)
-
-    return True 
-
-def lyon():
-
-    #Récupération de l'API
-    url="https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json?compact=false"
-
-    reponse = requests.get(url)
-    contenu=reponse.json()
-    infos_stations=contenu['values']
+        input_tab=input_station["fields"]["localisation"]
+        lilleTab1.append(input_tab)
 
     #Récupération des données de l'api
-    tab=[]
+    lilleTab=[]
+    i=0
     for input_station in infos_stations:
-        if(input_station["status"]=="OPEN"):
-            etat="EN SERVICE"
-        else:
-            etat="HORS SERVICE"
-        input_json={"ville":"Lyon","nomstation":input_station["pole"],"nombrevelo":input_station["available_bikes"],"nombreplaces":input_station["available_bike_stands"],"etat":etat,"latitude":input_station["lat"],"longitude":input_station["lng"]}
-        tab.append(input_json)
+        input_json={"ville":"Lille","nomstation":input_station["fields"]["nom"],"nombrevelo":input_station["fields"]["nbvelosdispo"],"nombreplaces":input_station["fields"]["nbplacesdispo"],"etat":input_station["fields"]["etat"],"location":{"type":"Point", "coordinates":lilleTab1[i]}}
+        lilleTab.append(input_json)
+        i=i+1
 
     #Ajout des données dans la BDD
-    exo2_collection.insert_many(tab)
-
+    exo2_collection.insert_many(lilleTab)  
 
     return True 
 
 def paris():
-        #Pour les vélos parisiens, nous avons besoin de 2 API différentes.
+    #Pour les vélos parisiens, nous avons besoin de 2 API différentes.
     #La récupération des données se fera donc en 2 étapes
 
     #Récupération de la première API (nom des stations, coordonnées)
@@ -67,34 +49,66 @@ def paris():
     infos_stations2=contenu['data']['stations']
 
     #Récupération des données spécifiques de la première api
-    tab1=[]
+    parisTab1=[]
     for input_station in infos_stations1:
         input_tab=[input_station["name"],input_station["lat"],input_station["lon"]]
-        tab1.append(input_tab)
+        parisTab1.append(input_tab)
 
     #Récupération des données spécifiques de la deuxième api
-    tab2=[]
+    parisTab2=[]
     for input_station in infos_stations2:
         if(input_station["is_renting"]==1):
             etat="EN SERVICE"
         else:
             etat="HORS SERVICE"
         input_tab=[input_station["num_bikes_available"],input_station["num_docks_available"],etat]
-        tab2.append(input_tab)
+        parisTab2.append(input_tab)
 
     #Concaténation des données provenant des 2 APIs
-    if(len(tab1)==len(tab2)):
-        tab=[]
-        for i in range (0,len(tab1)):
-            input_json={"ville":"Paris","nomstation":tab1[i][0],"nombrevelo":tab2[i][0],"nombreplaces":tab2[i][1],"etat":tab2[i][2],"latitude":tab1[i][1],"longitude":tab1[i][2]}
-            tab.append(input_json)
+    if(len(parisTab1)==len(parisTab2)):
+        parisTab=[]
+        for i in range (0,len(parisTab1)):
+            input_json={"ville":"Paris","nomstation":parisTab1[i][0],"nombrevelo":parisTab2[i][0],"nombreplaces":parisTab2[i][1],"etat":parisTab2[i][2],"location":{"type":"Point", "coordinates":[parisTab1[i][1],parisTab1[i][2]]}}
+            parisTab.append(input_json)
 
     #Ajout des données dans la BDD
-    exo2_collection.insert_many(tab)
+    exo2_collection.insert_many(parisTab)
 
     return True 
 
+def lyon():
+
+    # Récupération de l'API
+    url="https://download.data.grandlyon.com/ws/rdata/jcd_jcdecaux.jcdvelov/all.json?compact=false"
+
+    reponse = requests.get(url)
+    contenu=reponse.json()
+    infos_stations=contenu['values']
+
+    # Récupération des données de l'api
+
+    lyonTab1=[]
+    for input_station in infos_stations:
+        input_tab=[input_station["lat"],input_station["lng"]]
+        lyonTab1.append(input_tab)
+    i=0
+    lyonTab=[]
+    for input_station in infos_stations:
+        if(input_station["status"]=="OPEN"):
+            etat="EN SERVICE"
+        else:
+            etat="HORS SERVICE"
+        input_json={"ville":"Lyon","nomstation":input_station["pole"],"nombrevelo":input_station["available_bikes"],"nombreplaces":input_station["available_bike_stands"],"etat":etat,"location":{"type":"Point", "coordinates":lyonTab1[i]}}
+        lyonTab.append(input_json)
+        i=i+1
+
+    # Ajout des données dans la BDD
+    exo2_collection.insert_many(lyonTab)
+
+    return True
+
 def rennes():
+    
     #Récupération de l'API
     url="https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-stations-le-velo-star-en-temps-reel&q=&rows=-1&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles"
 
@@ -103,19 +117,27 @@ def rennes():
     infos_stations=contenu['records']
 
     #Récupération des données de l'api
-    tab=[]
+
+    rennesTab1=[]
+    for input_station in infos_stations:
+        input_tab=input_station["fields"]["coordonnees"]
+        rennesTab1.append(input_tab)
+
+    rennesTab=[]
+    i=0
     for input_station in infos_stations:
         if(input_station["fields"]["etat"]=="En fonctionnement"):
             etat="EN SERVICE"
         else:
             etat="HORS SERVICE"
-        input_json={"ville":"Rennes","nomstation":input_station["fields"]["nom"],"nombrevelo":input_station["fields"]["nombrevelosdisponibles"],"nombreplaces":input_station["fields"]["nombreemplacementsdisponibles"],"etat":etat,"latitude":input_station["fields"]["coordonnees"][0],"longitude":input_station["fields"]["coordonnees"][1]}
-        tab.append(input_json)
+        input_json={"ville":"Rennes","nomstation":input_station["fields"]["nom"],"nombrevelo":input_station["fields"]["nombrevelosdisponibles"],"nombreplaces":input_station["fields"]["nombreemplacementsdisponibles"],"etat":etat,"location":{"type":"Point", "coordinates":rennesTab1[i]}}
+        rennesTab.append(input_json)
+        i=i+1
 
     #Ajout des données dans la BDD
-    exo2_collection.insert_many(tab)
-    return True 
+    exo2_collection.insert_many(rennesTab)
 
+    return True
 
 #Connexion à la base mongo
 ConnectionString = "localhost:27017"
@@ -125,10 +147,16 @@ myClient = MongoClient(ConnectionString)
 myDB=myClient['Station']
 
 exo2_collection=myDB['Exo2']
+#test_col=myDB['nom']
+
+#Suppression des données précédentes
+exo2_collection.drop()
+exo2_collection.create_index([("location",GEOSPHERE)])
 
 for i in range(0,4):
 
     exo2_collection.drop()
+    exo2_collection.create_index([("location",GEOSPHERE)])
 
     lille()
     lyon()
--- exo3.py
+++ exo3.py
@@ -1,6 +1,5 @@
 # Import nécessaire
-from pymongo import GEO2D, GEOSPHERE, MongoClient
-from bson.son import SON
+from pymongo import  MongoClient, GEOSPHERE
 import pymongo
 from pymongo.message import query
 import requests
@@ -12,13 +11,23 @@ myClient = MongoClient(ConnectionString)
 #Initialisation des collections
 myDB=myClient['Station']
 
-exo1_collection=myDB['Exo1']
-# exo1_collection.create_index([("location",GEOSPHERE)])
+exo3_collection=myDB['Exo3']
+exo3_collection.drop()
+exo3_collection.create_index([("location",GEOSPHERE)])
 
-# lat = input("Entrer votre latitude:")
-# lon = input("Entrer votre longitude:")
+exo2_collection=myDB['Exo2']
 
-query = {"location":{"$near": { "$geometry": {"type":"Point", "coordinates":[45.6,2.343]}}}}
+cursor = exo2_collection.find({"etat":"EN SERVICE"})
 
-for doc in exo1_collection.find(query).limit(3):
+for doc in cursor:
+    myDB['Exo3'].insert_one(doc)
+
+
+
+lat = input("Entrer votre latitude:")
+lon = input("Entrer votre longitude:")
+
+query = {"location":{"$near": { "$geometry": {"type":"Point", "coordinates":[float(lat),float(lon)]}}}}
+
+for doc in exo3_collection.find(query).limit(3):
     print(doc)
